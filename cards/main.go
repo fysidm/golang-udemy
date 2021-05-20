155bad3889f365fae2aa620950de3aa0ead99647
GeorgeWu <fysidm@gmail.com>
2021-05-20 10:53:45 +0800

    Initialize cards

---
+++ cards/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// basic - initial variable
+	// var card string = "Ace of Spades"
+	// convention - initial variable
+	card := "Ace of Spades"
+
+	fmt.Println(card)
+}

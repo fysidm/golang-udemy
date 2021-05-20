6fe95554d65eb80239ecfcd054a53587d4c64b18
GeorgeWu <fysidm@gmail.com>
2021-05-20 14:50:43 +0800

    Custom type declaration and receiver function

--- cards/main.go
+++ cards/main.go
@@ -1,26 +1,24 @@
 package main
 
-import "fmt"
-
 func main() {
 	// basic - initial variable
 	// var card string = "Ace of Spades"
 	// var card string
 	// card = "Ace of Spades"
 	// convention - initial variable
-	card := newCard()
-
-	fmt.Println(card)
+	// card := newCard()
 
 	// Array: Fixed length array
 	// Slice: An array that cat grow or shrink
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
 	// For Loop
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
+	// for index, card := range cards {
+	// 	fmt.Println(index, card)
+	// }
+
+	cards.print()
 }
 
 func newCard() string {

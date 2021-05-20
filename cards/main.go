fd09d37f5df925f4ac64299c1af38d432c2e4ada
GeorgeWu <fysidm@gmail.com>
2021-05-20 11:22:14 +0800

    Array and for loop example

--- cards/main.go
+++ cards/main.go
@@ -5,10 +5,22 @@ import "fmt"
 func main() {
 	// basic - initial variable
 	// var card string = "Ace of Spades"
+	// var card string
+	// card = "Ace of Spades"
 	// convention - initial variable
 	card := newCard()
 
 	fmt.Println(card)
+
+	// Array: Fixed length array
+	// Slice: An array that cat grow or shrink
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	// For Loop
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
 }
 
 func newCard() string {

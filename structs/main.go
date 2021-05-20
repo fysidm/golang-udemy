9706c2a88f8a0171926467b14e227b037fae2f36
GeorgeWu <fysidm@gmail.com>
2021-05-20 18:32:22 +0800

    Embedding structs

--- structs/main.go
+++ structs/main.go
@@ -2,20 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipcode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var alex person
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipcode: 94000,
+		},
+	}
 
-	// Updating struct values
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-
-	// Log out alex
-	fmt.Println(alex)
-	// Check alex with struct key
-	fmt.Printf("%+v", alex)
+	fmt.Printf("%+v", jim)
 }
